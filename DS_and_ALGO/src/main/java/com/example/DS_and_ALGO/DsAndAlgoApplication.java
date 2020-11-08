53cd09387f471cc40d5412271d18896b3137dbcf
selva <selvakumar24896@gmail.com>
2020-11-08 12:16:09 +0530

    initial Commit

---
+++ DS_and_ALGO/src/main/java/com/example/DS_and_ALGO/DsAndAlgoApplication.java
@@ -0,0 +1,13 @@
+package com.example.DS_and_ALGO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsAndAlgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsAndAlgoApplication.class, args);
+	}
+
+}
---
+++ DS_and_ALGO/src/test/java/com/example/DS_and_ALGO/DsAndAlgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DS_and_ALGO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsAndAlgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
